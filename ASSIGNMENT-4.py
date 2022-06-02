e1aff24211f73bc56227601e4928e6198cca9203
SYED SAMEER <55532917+Sameer2357@users.noreply.github.com>
2022-06-02 23:15:15 +0530

    Add files via upload

---
+++ ASSIGNMENT-4.py
@@ -0,0 +1,45 @@
+#Write a Python program to create a lambda function that adds 25 to a given number passed in as an argument.
+
+lmd1 = lambda a : a + 25
+
+print(lmd1(int(input("sample input: "))))
+
+
+
+
+
+
+
+
+
+# 2 Write a Python program to triple all numbers of a given list of integers. Use Python map.
+
+
+# nums = (1, 2, 3, 4, 5, 6, 7) 
+# print("Original list: ", nums)
+# result = map(lambda x: x + x + x, nums) 
+# print("\nTriple of said list numbers:")
+# print(list(result))
+
+
+
+
+
+
+
+
+
+#  3  Write a Python program to square the elements of a list using map() function
+
+
+
+# def square_num(n):
+#   return n * n
+# nums = [4, 5, 2, 9]
+# print("Original List: ",nums)
+# result = map(square_num, nums)
+# print("Square the elements of the said list using map():")
+# print(list(result))
+
+
+
